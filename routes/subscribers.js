ce5796a4012461b88b5069ada8338600fdfc5344
raaj <amirraj20@gmail.com>
2021-01-13 16:42:52 +0600

    commit to master

---
+++ routes/subscribers.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+
+// Getting all
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+// Getting one
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+    
+})
+
+// Creating One
+router.post('/', (req, res) => {
+
+    
+})
+
+// updating One
+router.patch('/:id', (req, res) => {
+
+    
+})
+
+// Deleting One
+router.delete('/:id', (req, res) => {
+
+    
+})
+
+
+
+
+
+module.exports = router
